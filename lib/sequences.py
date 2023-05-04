ba36046a0ee9d20d04b310ed56ad83c48616c00b
Gabrielle Glasgow <g.glasgow91@gmail.com>
2023-05-04 19:59:46 -0400

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    if length == 1:
+        print([0])
+        return
+        
+
+    fib_seq = [0, 1]
+# common approach to fib sequence: iterative approach uses a loop
+    while len(fib_seq) < length:
+        next_num = fib_seq[-1] + fib_seq[-2]
+        fib_seq.append(next_num)
+
+    print(fib_seq)
